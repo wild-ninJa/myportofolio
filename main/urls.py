7cac36635d87a3078f6652348fabcda3d5f8619b
wild-ninJa <everleanskywalker@gmail.com>
2026-09-21 21:27:05 +0700

    Menambahkan fitur edit untuk experience dan project

--- main/urls.py
+++ main/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     path("projects/", show_projects, name="show_projects"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
     path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"),
+     path("projects/<uuid:project_id>/edit/", edit_project, name="edit_project"),
     path("experience/add/", create_experience, name="create_experience"),
     path("api/experience/", get_experiences_json, name="get_experiences_json"),
+    path("experience/<uuid:experience_id>/delete/", delete_experience, name="delete_experience"),
+    path("experience/<uuid:experience_id>/edit/", edit_experience, name="edit_experience"),
 ]
--- main/views.py
+++ main/views.py
@@ -72,6 +72,22 @@ def delete_experience(request, experience_id):
 
     return redirect("main:show_experience")
 
+def edit_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+    form = ExperienceForm(request.POST or None, instance=experience)
+    
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Experience updated!")
+        return redirect("main:show_experience")
+    
+    context = {
+        "name": "Husainah Syamsiah",
+        "form": form,
+        "experience": experience,
+    }
+    return render(request, "experience_form.html", context)
+
 # Skill area
 
 def show_skill(request):
@@ -133,3 +149,19 @@ def delete_project(request, project_id):
         return redirect("main:show_projects")
 
     return redirect("main:show_projects")
+
+def edit_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    form = ProjectForm(request.POST or None, instance=project)
+    
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Project is successfully updated!!")
+        return redirect("main:show_projects")
+    
+    context = {
+        "name": "Husainah Syamsiah",
+        "form": form,
+        "project": project,
+    }
+    return render(request, "projects_form.html", context)
